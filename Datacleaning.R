55e75c397f1c5eeb6177a6223971e9aafbfdb124
BernardoFalcao1 <145286544+BernardoFalcao1@users.noreply.github.com>
2023-10-24 09:31:53 -0400

    Add files via upload

---
+++ Datacleaning.R
@@ -0,0 +1,36 @@
+#Load Data
+library(readr)
+causeofdeath <- read_csv("~/Desktop/causeofdeath.csv")
+View(causeofdeath)
+
+#Check for missing values 
+sum(is.na(causeofdeath$Deaths))
+sum(is.na(causeofdeath$`Death Rate`))
+sum(is.na(causeofdeath$`Age Adjusted Death Rate`))
+
+#We need to clean the data 
+causeofdeath <- na.omit(causeofdeath)
+
+#We need to check columns for numeric
+class(causeofdeath$Deaths)
+class(causeofdeath$`Death Rate`)
+class(causeofdeath$`Age Adjusted Death Rate`)
+
+#Convert columns to numeric 
+causeofdeath$`Age Adjusted Death Rate`<-as.numeric(causeofdeath$`Age Adjusted Death Rate`)
+causeofdeath$`Death Rate`<- as.numeric(causeofdeath$`Death Rate`)
+causeofdeath$Deaths<- as.numeric(causeofdeath$Deaths)
+
+#Check for missing values 
+sum(is.na(causeofdeath$Deaths))
+sum(is.na(causeofdeath$`Death Rate`))
+sum(is.na(causeofdeath$`Age Adjusted Death Rate`))
+
+#We need to clean the data 
+causeofdeath <- na.omit(causeofdeath)
+
+#We need to check columns for numeric
+class(causeofdeath$Deaths)
+class(causeofdeath$`Death Rate`)
+class(causeofdeath$`Age Adjusted Death Rate`)
+
