5f7be19009169cd4c243c51268bfb4fbc068a38d
BernardoFalcao1 <145286544+BernardoFalcao1@users.noreply.github.com>
2023-10-29 20:46:11 -0400

    Add files via upload

---
+++ ConfidenceIntPart1.R
@@ -0,0 +1,27 @@
+# Mean of the population of Deaths 
+xbar<- mean(Newcauseofdeath$Deaths)
+
+# Standard deviation of Deaths
+s<-  sd(Newcauseofdeath$Deaths)
+
+# Sample size
+n<- length(Newcauseofdeath$Deaths)
+ 
+#Calculate the Z-interval
+z<- -qnorm(0.05/2)
+
+#Calculate the Lower & Upper bound in the z-interval
+Lz<- xbar- z*s/sqrt(n)
+Uz<- xbar+ z*s/sqrt(n)
+
+#Print
+cat("The Upper and Lower bounds of Z interval is [", Lz, ",", Uz, "]")
+
+#Calculate the t- interval 
+t<- -qt(0.025,n-1)
+
+#Calculate the Lower & Upper bound in the t-interval
+Lt<- xbar- t*s/sqrt(n)
+Ut<- xbar+ t*s/sqrt(n)
+
+cat("The Upper and Lower bounds of Z interval is [", Lt, ",", Ut, "]")
